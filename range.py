9201422c0f9d12d66ff9c0e3e12f8d16b4fc6ed7
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-01 00:00:04 -0700

    Add files via upload

---
+++ range.py
@@ -0,0 +1,24 @@
+# for i in range(2):
+#     print(i)
+ 
+ 
+# for m in range(1):
+#     print(m)
+    
+# for n in range(5):
+#     print(n)
+    
+# for l in range(6):
+#     print(l)
+    
+    
+# for v in range(1,3,1):
+#     print(v)
+    
+
+
+
+
+for f in range(0,10,2):
+    print(f)
+    
